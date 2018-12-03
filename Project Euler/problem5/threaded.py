cbb1f9dc4c70101d48e2c1e09f528954b5bd628e
Turki Aamer A Alrifaie <alrifaie.t@ICD25WM0RJJ1GN.tld>
2018-12-03 13:13:10 -0500

    pushed

---
+++ Project Euler/problem5/threaded.py
@@ -0,0 +1,7 @@
+# @Author: Turki A Alrifaie
+# @Date:   2018-12-03T11:39:17-05:00
+# @Email:  alrifaie.t@husky.neu.edu
+# @Filename: threaded.py
+# @Last modified by:   Turki A Alrifaie
+# @Last modified time: 2018-12-03T13:11:57-05:00
+# @License: Free
