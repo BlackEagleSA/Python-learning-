7f3153f0c86cd34247ec9dac25623ce4268a4c01
Turki Alrifaie <turkiaamer@gmail.com>
2021-08-09 12:11:57 +0300

    fixed bugs

--- Data Science/London Olympics/London Olympics.py
+++ Data Science/London Olympics/London Olympics.py
@@ -2,8 +2,8 @@
 import numpy as np
 
 # importing the data set manually as arrays 
-country = np.array([Great Britain, China, Russia, United States, Korea, Japan, Germany])
-coutrny_code = np.array([GBR, CHN, RUS, US, KOR, JPN, GER])
+country = np.array(["Great Britain", "China", "Russia", "United States", "Korea", "Japan", "Germany"])
+coutrny_code = np.array(["GBR", "CHN", "RUS", "US", "KOR", "JPN", "GER"])
 year = np.array([2012,2012,2012,2012,2012,2012,2012])
 Gold = np.array([29, 38, 24, 46, 13, 7, 11])
 Silver = np.array([17, 28, 25, 28, 8, 14, 11])
