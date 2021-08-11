51498df8f52907257ad0c954a7a3dee53f35eb80
Turki Alrifaie <turkiaamer@gmail.com>
2021-08-11 17:31:54 +0300

    -

--- Data Science/FDA/FDA.py
+++ Data Science/FDA/FDA.py
@@ -2,5 +2,12 @@ import numpy as np
 import pandas as pd 
 
 cin = pd.read_csv(r'C:\Users\turalrif\OneDrive - Crayon Group\Python-learning-\Data Science\FDA\faa_ai_prelim.csv')
-print(cin)
+# print(cin.dtypes)
 
+# print(cin[:5])
+# print(cin.columns)
+
+cols = ['ACFT_MAKE_NAME', 'ACFT_MODEL_NAME', 'LOC_STATE_NAME', 'RMK_TEXT', 'FLT_PHASE', 'EVENT_TYPE_DESC', 'FATAL_FLAG']
+extract  = cin[cols]
+# print(extract)
+print(extract.dtypes)
